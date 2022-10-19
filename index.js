141952fa9bca69da01eedde895442c149185c523
faizan <faizanzaid789@gmail.com>
2022-10-19 06:22:18 +0530

    bug fixes

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ mongooseConnect();
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
 app.get('/', (req,res)=>{
-	req.json({success:'success'})
+	res.json({success:'success'})
 })
 
 
